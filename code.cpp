e32a41de0d27f39054e0a2c10b79e9deb8131e52
Yash Kumar <info.yashkumar.09@gmail.com>
2026-08-20 22:48:56 +0530

    Day-01

---
+++ code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // cout << "Yash Kumar";
+    float PI = 3.14f;
+    // cout << PI;
+    int a = 33000;
+    short b = a;
+    // cout << a << endl;
+    // cout << b;
+    double y = 13.335656;
+    float x = y;
+    // cout << y;
+
+    int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // cout << age;
+
+    return 0;
+}
