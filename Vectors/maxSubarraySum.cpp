9293379ddf259dceb6f3737909dd855e3bcf2da8
Yash Kumar <info.yashkumar.09@gmail.com>
2026-09-03 21:25:06 +0530

    Day 13

--- Vectors/maxSubarraySum.cpp
+++ Vectors/maxSubarraySum.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n=5;
-    int arr[n] = {1, -2, 3, 4, -5};
+    int arr[n] = {1, -2, 3, -4, 7, 1, -5};
     int maxSum = 0;
     for(int st=0; st<n; st++){
         int currentSum = 0;
