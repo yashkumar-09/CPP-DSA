582bcc9d3ba77090fd0ba3c6f96aa1511a0a6731
Yash Kumar <info.yashkumar.09@gmail.com>
2026-09-05 14:03:29 +0530

    Day 14

--- Vectors/pairSumBruteForce.cpp
+++ Vectors/pairSumBruteForce.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n = 4, target = 9;
-    int arr[] = {2, 7, 11, 15};
+vector<int> pairSum(vector<int> nums, int target){
+    vector<int> ans;
+    int n = nums.size();
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
-            if(arr[i]+arr[j]==target){
-                cout<<"("<<i<<","<<j<<")"; //for idx
-                cout<<"("<<arr[i]<<","<<arr[j]<<")"; //for elements on idx
+            if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
             }
         }
     }
+    return ans;
+}
+int main(){
+    vector<int> nums = {5, 7, 11, 4, 15, 2, 7};
+    int target = 18;
+    vector<int> ans = pairSum(nums, target);
+    cout<<"("<<ans[0]<<","<<ans[1]<<")"<<endl;
     return 0;
 }
